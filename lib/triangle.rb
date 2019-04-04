0fd42c8e5d5241c6dfea245b96092fd8ca62f056
Carlton Dwayne <carltondgrandison@gmail.com>
2019-04-04 04:15:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,16 +9,16 @@ class Triangle
   end 
   
   #def kind 
-    if @side_1 == @side_2 && @side_2 == @side_3 
-      @type = :equilateral 
-    elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_3 == @side_1
-      @type = :isosceles
-    elsif @side_1 > @side_2 || @side_2 > @side_3 || @side_3 > @side_1 
-      @type = :scalene
-    elsif @side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0 
-      raise TriangleError 
-    else @side_1 + @side_2 <= @side_3 || @side_2 + @side_3 <= @side_1 || @side_1 + @side_3 <= @side_2
-      raise TriangleError
-    end 
-  end 
+    #if @side_1 == @side_2 && @side_2 == @side_3 
+     # @type = :equilateral 
+    #elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_3 == @side_1
+    #  @type = :isosceles
+   # elsif @side_1 > @side_2 || @side_2 > @side_3 || @side_3 > @side_1 
+   #   @type = :scalene
+  #  elsif @side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0 
+  #    raise TriangleError 
+ #   else @side_1 + @side_2 <= @side_3 || @side_2 + @side_3 <= @side_1 || @side_1 + @side_3 <= @side_2
+ #     raise TriangleError
+ #   end 
+#  end 
 end
