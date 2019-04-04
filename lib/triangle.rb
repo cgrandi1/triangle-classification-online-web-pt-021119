3ea8c05b24500e1c2ccf1abed602ef390a95fb0e
Carlton Dwayne <carltondgrandison@gmail.com>
2019-04-04 03:44:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,9 +8,11 @@ class Triangle
   end 
   
   def kind 
-    if @side_1 == @side_2 && @side_2 == @side_3 
+    if 
+      @side_1 == @side_2 && @side_2 == @side_3 
       @type = :equilateral 
     elsif 
+    
   end 
   
 end
