884a2db9ce9bce9fa0e40cb451ada23400c51e86
Carlton Dwayne <carltondgrandison@gmail.com>
2019-04-04 03:54:23 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,7 @@ class Triangle
       @type = :isosceles
     elsif @side_1 > @side_2 || @side_2 > @side_3 || @side_3 > @side_1 
       @type = :scalene
-    else @side_1 == 0 || @side_2 == 0 || @side_3 == 0 
+    else @side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0 
       raise TriangeError 
     end 
   end 
