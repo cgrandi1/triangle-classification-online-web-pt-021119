5aa44864bcc4d9e4740f3dba8f81f9aadafede7d
Carlton Dwayne <carltondgrandison@gmail.com>
2019-04-04 04:32:46 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,31 +1,32 @@
-class Triangle 
-  
- 
- attr_accessor :side_1, :side_2, :side_3, :type
- 
+class Triangle
+
+  attr_accessor :side_1, :side_2, :side_3, :type
+
   def initialize(side_1, side_2, side_3)
     @side_1 = side_1
     @side_2 = side_2
-    @side_3 = side_3 
-  end 
-  
-  def kind 
-    if @side_1 == @side_2 && @side_2 == @side_3 
-      @type = :equilateral 
-    elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_3 == @side_1
-      @type = :isosceles
-    elsif @side_1 > @side_2 || @side_2 > @side_3 || @side_3 > @side_1 
-      @type = :scalene
-    elsif @side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0 
-      raise TriangleError 
-    else @side_1 + @side_2 <= @side_3 || @side_2 + @side_3 <= @side_1 || @side_1 + @side_3 <= @side_2
-      raise TriangleError
-    end 
-  end 
-  
-  class TriangleError < StandardError
-    def message 
-      
-    end 
+    @side_3 = side_3
   end
-end 
+
+
+  def kind
+    if @side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0
+      raise TriangleError
+    elsif @side_1 + @side_2 <= @side_3 || @side_2 + @side_3 <= @side_1 || @side_3 + @side_1 <= @side_2
+      raise TriangleError
+    elsif @side_1 == @side_2 && @side_2 == @side_3
+      @type = :equilateral
+    elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3
+      @type = :isosceles
+    else
+      @type = :scalene
+    end
+
+  end
+
+
+  class TriangleError < StandardError
+    def message
+
+    end
+  end
