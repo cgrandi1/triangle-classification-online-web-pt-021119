d195dce1a37166705bb91c846d44f231c1a0a277
Carlton Dwayne <carltondgrandison@gmail.com>
2019-04-04 04:21:55 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,7 +17,7 @@ class Triangle
       @type = :scalene
     elsif @side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0 
       raise TriangleError 
-    elsif @side_1 + @side_2 <= @side_3 || @side_2 + @side_3 <= @side_1 || @side_1 + @side_3 <= @side_2
+    else @side_1 + @side_2 <= @side_3 || @side_2 + @side_3 <= @side_1 || @side_1 + @side_3 <= @side_2
       raise TriangleError
     end 
   end 
